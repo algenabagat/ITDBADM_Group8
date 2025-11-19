14421407c63f156ac98c3f975c0186eeb0dc600a
algen_abagat <144084800+algenabagat@users.noreply.github.com>
2025-11-19 16:44:11 +0800

    final

--- config.php
+++ config.php
@@ -1,7 +1,6 @@
 <?php 
-/* Change my_username/my_password of ur database here */
 $servername = "127.0.0.1";
-$username = "casem_code"; //used casem_code to access the sql for the website and student2 for workbench concurrent usage
+$username = "student1"; 
 $password = "Dlsu1234!";
 $database = "watch_db";
 $port = 3309;
